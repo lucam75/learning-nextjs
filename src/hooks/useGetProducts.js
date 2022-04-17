ad91a626386d6f21c50741f7171867cc5c677c57
Luis Campos <lucam75@gmail.com>
2022-04-17 12:45:28 -0400

    fixing build errors.

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -1,22 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const useGetProducts = (API) => {
-	const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-	useEffect(() => {
-		async function fetchData() {
-		  // You can await here
-		  const response = await axios(API);
-		  const filteredProducts = response.data.filter(product => {
-			  return product.images.length > 0;
-		  })
-		  setProducts(filteredProducts);
-		}
-		fetchData();
-	  }, []);
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      const response = await axios(API);
+      const filteredProducts = response.data.filter((product) => {
+        return product.images.length > 0;
+      });
+      setProducts(filteredProducts);
+    }
+    fetchData();
+  }, [API]);
 
-	  return products;
+  return products;
 };
 
 export default useGetProducts;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>React Shop</title>
+          <meta charSet="utf-8" />
         </Head>
         <body>
           <Main />
