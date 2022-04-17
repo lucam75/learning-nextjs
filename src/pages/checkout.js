a9db2f814428f6231fc37e7703f73bbfc6f6d5b3
Luis Campos <lucam75@gmail.com>
2022-04-17 12:11:11 -0400

    Adding support to head and title

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,24 +1,30 @@
 import React from 'react';
+import Head from 'next/head';
 import OrderItem from '@components/OrderItem';
 
 const Checkout = () => {
-	return (
-		<div className="Checkout">
-			<div className="Checkout-container">
-				<h1 className="title">My order</h1>
-				<div className="Checkout-content">
-					<div className="order">
-						<p>
-							<span>03.25.21</span>
-							<span>6 articles</span>
-						</p>
-						<p>$560.00</p>
-					</div>
-				</div>
-				<OrderItem />
-			</div>
-		</div>
-	);
-}
+  return (
+    <>
+      <Head>
+        <title>Checkout</title>
+      </Head>
+      <div className="Checkout">
+        <div className="Checkout-container">
+          <h1 className="title">My order</h1>
+          <div className="Checkout-content">
+            <div className="order">
+              <p>
+                <span>03.25.21</span>
+                <span>6 articles</span>
+              </p>
+              <p>$560.00</p>
+            </div>
+          </div>
+          <OrderItem />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Checkout;
