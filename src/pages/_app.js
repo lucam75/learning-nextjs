e5853e42ecf7f91087d27f40951be28948afcf69
Luis Campos <lucam75@gmail.com>
2022-04-16 21:36:27 -0400

    Putting together React project and nextjs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import '@styles/Header.scss';
+import '@styles/Menu.scss';
+import '@styles/MyOrder.scss';
+import '@styles/OrderItem.scss';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
